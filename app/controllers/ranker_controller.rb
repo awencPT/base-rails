95dab48a401b0fb299bab41ee3a825c64ae0b2ca
awencPT <awenc@chicagobooth.edu>
2023-01-09 21:17:31 +0000

    token

--- app/controllers/ranker_controller.rb
+++ app/controllers/ranker_controller.rb
@@ -1,5 +1,6 @@
 class RankerController < ActionController::Base
-  skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :exception
+  #skip_before_action :verify_authenticity_token
 
   def poolView
     #  string = Analysis.all.last.final_analysis
