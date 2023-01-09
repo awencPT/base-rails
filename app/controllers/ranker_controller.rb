dfd66e3071e3736a341122ce0d13cf22a72d56a8
awencPT <awenc@chicagobooth.edu>
2023-01-09 21:42:54 +0000

    cagadfg

--- app/controllers/ranker_controller.rb
+++ app/controllers/ranker_controller.rb
@@ -1,6 +1,6 @@
 class RankerController < ActionController::Base
   protect_from_forgery with: :exception
-  #skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 
   def poolView
     #  string = Analysis.all.last.final_analysis
--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,5 +1,5 @@
 class StatisticsController < ActionController::Base
-  # skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   protect_from_forgery with: :exception
 
   def home
